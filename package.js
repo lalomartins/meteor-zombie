7d24e75dd0c3042713aa2f174b8274c0fb7c49bf
Lalo Martins <lalo.martins@gmail.com>
2015-10-29 11:18:35 +0100

    initial commit

---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+  summary: "make zombie (fast headless browser) available on the server",
+  name: "lalomartins:zombie",
+  version: "0.1.0",
+  git: "https://github.com/lalomartins/meteor-zombie.git"
+});
+
+Npm.depends({
+  'zombie': '4.0.8'
+});
+
+Package.onUse(function(api) {
+  api.addFiles('wrap.js', 'server');
+  api.export('Zombie', 'server');
+});
---
+++ wrap.js
@@ -0,0 +1,1 @@
+Zombie = Npm.require('zombie');
